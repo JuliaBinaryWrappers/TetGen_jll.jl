b2a24b84c0324b1c4bfce4797d80efbd158f291c
jlbuild <juliabuildbot@gmail.com>
2026-05-10 20:00:08 +0000

    TetGen_jll build 1.6.0+0

--- src/TetGen_jll.jl
+++ src/TetGen_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("TetGen")
-JLLWrappers.@generate_main_file("TetGen", UUID("b47fdcd6-d2c1-58e9-bbba-c1cee8d8c179"))
+JLLWrappers.@generate_main_file("TetGen", Base.UUID("b47fdcd6-d2c1-58e9-bbba-c1cee8d8c179"))
 end  # module TetGen_jll
